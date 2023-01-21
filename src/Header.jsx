c85a8bed2c347bff122281d1adad4472e349c960
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2023-01-21 11:24:24 -0300

    Header
    Estrutura básica do Header criada.
    
    O css não é encontrado pelo Reactjs.
    
    Ajuste necessário antes de prosseguir.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,27 @@
+import React from 'react';
+import './Header.css';
+
 function Header() {
     return (
-        <div>
-            <div>
-                HEADER
+        <header>
+            <div className="header__logo-container">
+                <img className="header__logo" src="logo.png" alt="Logo" />
             </div>
 
-        </div>
+            <div className="header__search-container">
+                <input className="header__search" type="text" placeholder="Search" />
+            </div>
+
+            <nav className="header__nav-container">
+                <ul className="header__nav-list">
+                    <li className="header__nav-item">Home</li>
+
+                    <li className="header__nav-item">About</li>
+
+                    <li className="header__nav-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
