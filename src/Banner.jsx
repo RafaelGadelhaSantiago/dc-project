84dfe14141f19f60a2871e27e06065925270b26e
RogerioBrasileiro <105891672+RogerioBrasileiro@users.noreply.github.com>
2023-01-21 13:34:52 -0300

    Update Banner.jsx

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,9 +1,11 @@
 function Banner() {
     return (
         <div>
-            <div>
-                BANNER
+            <div>Melhores ofertas personalizadas             
             </div>
+            <div>Queima de estoque Nike</div>
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat enim dolores, ducimus voluptates beatae porro non? Beatae perferendis obcaecati at, fuga eius facere repellat atque iusto quia tempora, sit necessitatibus.</div>
+            <div><input type="button" value="Ver ofertas" /></div>
 
         </div>
     );
