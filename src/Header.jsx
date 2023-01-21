736e714028b42e67dc14d15f25134e8883dd00f1
Rafael <99349794+RafaelGadelhaSantiago@users.noreply.github.com>
2023-01-21 16:43:41 -0300

    Correção CSS
    CSS corrigido.
    
    Alguns estilos foram aplicados para dar a forma geral do que será o Header.
    
    Será necessário agora ajustar tamanhos, cores e efeitos hover.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import './Header.css';
+import CSS from './Header.css';
 
 function Header() {
     return (
-        <header>
-            <div className="header__logo-container">
-                <img className="header__logo" src="logo.png" alt="Logo" />
+        <header className='header'>
+            <div className="header_top">
+                <div className='header__logo-container'>
+                    <img className='header__logo' src="logo.png" alt="Logo" />
+                </div>
+
+                <div className='header__search-container'>
+                    <input className='header__search' type="text" placeholder="Search" />
+                </div>
+
+                <div className="header_link-conteiner">
+                    <a href="/">Cadastre-se</a>
+
+                    {/* será necessário cirar o componente BOTAO */}
+                    <a className='header-btn' href="/">ENTRAR</a>
+                </div>
+
+                <div className="header_kart-conteiner">
+                    <img className="header_kart" src="logo.png" alt="Logo" />
+                </div>
             </div>
 
-            <div className="header__search-container">
-                <input className="header__search" type="text" placeholder="Search" />
+            <div className="header_bottom">
+                <nav className='header__nav-container'>
+                    <ul className='header__nav-menu'>
+                        <li className='header__nav-item'>Home</li>
+
+                        <li className='header__nav-item'>Produtos</li>
+
+                        <li className='header__nav-item'>Categorias</li>
+
+                        <li className='header__nav-item'>Meus Produtos</li>
+                    </ul>
+                </nav>
             </div>
-
-            <nav className="header__nav-container">
-                <ul className="header__nav-list">
-                    <li className="header__nav-item">Home</li>
-
-                    <li className="header__nav-item">About</li>
-
-                    <li className="header__nav-item">Contact</li>
-                </ul>
-            </nav>
-        </header>
+        </header >
     );
 }
 
