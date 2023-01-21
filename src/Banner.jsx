f697037da0594b08e202112d0d617a9b1cf39872
RogerioBrasileiro <105891672+RogerioBrasileiro@users.noreply.github.com>
2023-01-21 14:58:30 -0300

    update banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -11,10 +11,38 @@ function Banner() {
             }}>
 
             </div>
-            <div>Melhores ofertas personalizadas             
+            <div style={{
+                color: 'red',
+                width: '100%',
+                border: '1px solid blue',
+                display: 'flex',
+                justifyContent: 'flex',
+                flexDirection: 'column'
+
+            }}>
+                <h4>Melhores ofertas personalizadas</h4>
             </div>
-            <div>Queima de estoque Nike</div>
-            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat enim dolores, ducimus voluptates beatae porro non? Beatae perferendis obcaecati at, fuga eius facere repellat atque iusto quia tempora, sit necessitatibus.</div>
+            <div style={{
+                color: 'red',
+                width: '100%',
+                border: '1px solid blue',
+                display: 'flex',
+                justifyContent: 'flex',
+                flexDirection: 'column'
+
+            }}>
+                <h1>Queima de estoque Nike</h1></div>
+
+            <div style={{
+                color: 'red',
+                width: '100%',
+                border: '1px solid blue',
+                display: 'flex',
+                justifyContent: 'flex',
+                flexDirection: 'column'
+
+            }}><h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat enim dolores, ducimus voluptates beatae porro non? Beatae perferendis obcaecati at, fuga eius facere repellat atque iusto quia tempora, sit necessitatibus.</h5></div>
+
             <div><input type="button" value="Ver ofertas" /></div>
 
         </div>
