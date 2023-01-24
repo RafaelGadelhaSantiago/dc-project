3d94846d248fca48e4c3d06d588a898c961a7e97
Adriana sm <a.s.m27267@gmail.com>
2023-01-24 20:28:46 -0300

    atualizando footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,14 @@
-// import "./footer.css"
+import "./footer.css"
 import Digitalogo from './Digitalogo.svg'
 function Footer() {
 
     return (
         <div className="fundo">
             <div>
-          <img className="dc" src={Digitalogo} alt="" />  Digital Store
+          <img className="dc" src={Digitalogo} alt="" /> Digital Store
             </div>
-            <div className="">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ullam minima, aliquam architecto magni ratione dolore maiores iste quod velit, corporis culpa harum sint aperiam corrupti saepe! Aspernatur, fugit nesciunt.</p> 
+            <div >
+                <p className="descricao">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit ullam minima, aliquam architecto magni ratione dolore maiores iste quod velit, corporis culpa harum sint aperiam corrupti saepe! Aspernatur, fugit nesciunt.</p> 
             </div>
 
         </div>
