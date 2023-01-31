af66bb74ff54af67e1ab9a781b135a3f258c04c9
LeonardoMeireles <leomeireles69@hotmail.com>
2023-01-31 20:46:17 -0300

    update

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -9,7 +9,15 @@ function Banner() {
             <div class="info"><h4>Consequat culpa exercitation mollit nisi excepteur do
                 do tempor laboris eiusmod irure consectetur.</h4></div>
 
-            <div></div>
+            <div style={{
+                color: 'red',
+                width: '100%',
+                border: '1px solid blue',
+                display: 'flex',
+                justifyContent: 'flex',
+                flexDirection: 'column'
+
+            }}><h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat enim dolores, ducimus voluptates beatae porro non? Beatae perferendis obcaecati at, fuga eius facere repellat atque iusto quia tempora, sit necessitatibus.</h5></div>
 
 
         </div>
