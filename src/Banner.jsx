409490d9e8e9a5bfb9c9b0754863ce40818be311
LeonardoMeireles <leomeireles69@hotmail.com>
2023-01-31 19:51:08 -0300

    Update Banner.jsx

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -6,7 +6,11 @@ function Banner() {
 
             <div class="info"><h1>Queima de Estoque Nike 🔥</h1></div>
 
-            <div class="info"></div>
+            <div class="info"><h4>Consequat culpa exercitation mollit nisi excepteur do
+                do tempor laboris eiusmod irure consectetur.</h4></div>
+
+            <div></div>
+
 
         </div>
     );
