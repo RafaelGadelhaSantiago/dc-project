226509f179de0921e56fe41c49bc352fc7331507
Adriana <a.s.m27267@gmail.com>
2023-02-01 23:24:38 -0300

    Iniciando o carrinho

---
+++ src/CarrinhoCompra.jsx
@@ -0,0 +1,31 @@
+function CarrinhoCompra() {
+    return <div style={{
+        position:'absolute',
+        backgroundColor: 'white',
+        border: '3px solid black',
+        borderRadius:'10px',
+        height: '454px',
+        width:'315px',
+        display:'flex',
+        justifyContent: 'center',
+    }}>
+        <h1 style={{
+            borderBottom: '1px solid black',
+            padding:'10px',
+            fontSize: '16px',
+        }}>Meu carrinho</h1>
+    
+        <div style={{}
+        }> 
+
+            <div >
+                Tenis
+            </div>
+            <div>
+                Preço
+            <div/>        
+        </div>
+        </div>
+        </div>
+}
+export default CarrinhoCompra
